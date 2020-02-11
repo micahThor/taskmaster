812a645bfdba8d55f1f06a0c7b496b1f7c8b7d85
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2020-02-11 10:36:00 -0800

    completed lab 26 requirements

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // wire up add tasks button
+        Button goToAddTasksButton = findViewById(R.id.goToTaskActivityButton);
+        goToAddTasksButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent goToAddTaskActivity = new Intent(MainActivity.this, addTask.class);
+                MainActivity.this.startActivity(goToAddTaskActivity);
+            }
+        });
+
+        // wire up all tasks button
+        Button goToAllTasksButton = findViewById(R.id.allTasksButton);
+        goToAllTasksButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent goToAllTaskActivity = new Intent(MainActivity.this, allTasks.class);
+                MainActivity.this.startActivity(goToAllTaskActivity);
+            }
+        });
     }
 }
