d4981c82b4ff9318b29fd24286131623dc294b2e
Thorstenson <micathor@38f9d36a23ef.ant.amazon.com>
2020-02-12 11:13:22 -0800

    met lab 27 requirements

--- app/src/main/java/com/micahThor/taskmaster/MainActivity.java
+++ app/src/main/java/com/micahThor/taskmaster/MainActivity.java
@@ -3,9 +3,13 @@ package com.micahThor.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // wire up add tasks button
+        // wire up ADD TASKS BUTTON
         Button goToAddTasksButton = findViewById(R.id.goToTaskActivityButton);
         goToAddTasksButton.setOnClickListener(new View.OnClickListener() {
 
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // wire up all tasks button
+        // wire up ALL TASKS BUTTON
         Button goToAllTasksButton = findViewById(R.id.allTasksButton);
         goToAllTasksButton.setOnClickListener(new View.OnClickListener() {
 
@@ -36,5 +40,74 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(goToAllTaskActivity);
             }
         });
+
+        // wire up SETTINGS BUTTON
+        ImageButton settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent goToSettingsActivity = new Intent(MainActivity.this, SettingsActivity.class);
+                MainActivity.this.startActivity(goToSettingsActivity);
+            }
+        });
+
+        /*
+                THREE HARD-CODED TASK BUTTONS
+         */
+        final Button washDishesButton = findViewById(R.id.task1Button);
+        washDishesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // get text of button to pass into shared preferences
+                String taskTitle = washDishesButton.getText().toString();
+                saveTitleToSharedPrefs(taskTitle);
+            }
+        });
+
+        final Button walkMolly = findViewById(R.id.task2Button);
+        walkMolly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // get text of button to pass into shared preferences
+                String taskTitle = walkMolly.getText().toString();
+                saveTitleToSharedPrefs(taskTitle);
+            }
+        });
+
+        final Button buyFlowers = findViewById(R.id.task3Button);
+        buyFlowers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // get text of button to pass into shared preferences
+                String taskTitle = buyFlowers.getText().toString();
+                saveTitleToSharedPrefs(taskTitle);
+            }
+        });
+
+    }
+
+    private void saveTitleToSharedPrefs(String title) {
+
+        SharedPreferences sharedPrefers = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = sharedPrefers.edit();
+        editor.putString("taskTitle", title);
+        editor.commit();
+        Intent goToAllTaskActivity = new Intent(MainActivity.this, TaskDetailActivity.class);
+        MainActivity.this.startActivity(goToAllTaskActivity);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        TextView userNameTextView = findViewById(R.id.userNameTextView);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String userName = sharedPrefs.getString("userName", "User Name");
+        userNameTextView.setText(userName + "'s Tasks");
     }
 }
